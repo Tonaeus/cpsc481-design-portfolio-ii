f5a6fbe2d9ff05ba5e14a2b1872baa3ce0434196
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-10-14 23:23:20 -0600

    stylized the account page

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,10 +1,113 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+	Card,
+	Tabs,
+	TextInput,
+	PasswordInput,
+	Button,
+	Group,
+	Text,
+	FloatingIndicator,
+} from "@mantine/core";
 
 const Account = () => {
 	useEffect(() => {
 		document.title = "Account";
 	}, []);
-	return <div>Account</div>;
+
+	const [rootRef, setRootRef] = useState(null);
+	const [value, setValue] = useState("1");
+	const [controlsRefs, setControlsRefs] = useState({});
+	const setControlRef = (val) => (node) => {
+		controlsRefs[val] = node;
+		setControlsRefs(controlsRefs);
+	};
+
+	return (
+		<div className="h-full flex justify-center items-center">
+			<Card
+				shadow="xs"
+				radius="lg"
+				padding="xl"
+				withBorder
+				className="w-sm"
+			>
+				<Tabs
+					variant="none"
+					value={value}
+					onChange={setValue}
+					className="relative"
+				>
+					<Tabs.List ref={setRootRef} className="flex w-full">
+						<Tabs.Tab value="1" ref={setControlRef("1")} className="flex-1">
+							Sign In
+						</Tabs.Tab>
+						<Tabs.Tab value="2" ref={setControlRef("2")} className="flex-1">
+							Sign Up
+						</Tabs.Tab>
+
+						<FloatingIndicator
+							target={value ? controlsRefs[value] : null}
+							parent={rootRef}
+							className="bg-white rounded-md border border-gray-200 shadow-sm"
+						/>
+					</Tabs.List>
+
+					<Tabs.Panel value="1">
+						<TextInput
+							label="Email"
+							placeholder="you@example.com"
+							required
+							mb="sm"
+						/>
+						<PasswordInput
+							label="Password"
+							placeholder="••••••••"
+							required
+							mb="sm"
+						/>
+						<Group justify="space-between" mt="md">
+							<Button fullWidth>
+								Sign in
+							</Button>
+						</Group>
+						<Text size="sm" mt="sm" c="dimmed" ta="center">
+							Forgot password?
+						</Text>
+					</Tabs.Panel>
+					<Tabs.Panel value="2">
+						<TextInput
+							label="Full Name"
+							placeholder="John Doe"
+							required
+							mb="sm"
+						/>
+						<TextInput
+							label="Email"
+							placeholder="you@example.com"
+							required
+							mb="sm"
+						/>
+						<PasswordInput
+							label="Password"
+							placeholder="••••••••"
+							required
+							mb="sm"
+						/>
+						<PasswordInput
+							label="Confirm Password"
+							placeholder="••••••••"
+							required
+							mb="sm"
+						/>
+						<Button fullWidth mt="md">
+							Sign up
+						</Button>
+					</Tabs.Panel>
+				</Tabs>
+			</Card>
+		</div>
+	);
 };
 
 export default Account;
