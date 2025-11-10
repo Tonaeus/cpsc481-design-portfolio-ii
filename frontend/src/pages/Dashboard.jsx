9caa8832294e53fee797349716774b7b39abcab4
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-11-09 22:46:55 -0700

    added a title to each dashboard section

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,22 +2,12 @@ import { useState, useEffect } from "react";
 import {
 	Anchor,
 	Card,
-	Button,
-	Stack,
 	Title,
-	Text,
 	ScrollArea,
 	Table,
 	Badge,
 } from "@mantine/core";
-import {
-	BookOpen,
-	History,
-	CreditCard,
-	BarChart3,
-	FileText,
-	Info,
-} from "lucide-react";
+import { Info } from "lucide-react";
 import { Link, useNavigate } from "react-router";
 import useAuthContext from "../hooks/useAuthContext";
 import { getTransactionsWithBookInfo } from "../../../backend/history.jsx";
@@ -68,14 +58,20 @@ const Dashboard = () => {
 	return (
 		<div className="h-full flex flex-col gap-4">
 			<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+				{/* Welcome */}
 				<Card withBorder className="md:col-span-3">
 					<Title order={3}>
 						Welcome,{" "}
 						{user ? `${user.first_name} ${user.last_name}` : "Library Member"}!
 					</Title>
 				</Card>
-
-				<Card withBorder className="md:col-span-2">
+				
+				{/* Borrowed Items */}
+				<Card withBorder className="h-80 md:col-span-2">
+					<Title order={4} >
+						Borrowed Items
+					</Title>
+					<div className="h-[1px] my-4 bg-gray-200" />
 					<ScrollArea className="h-full">
 						<Table stickyHeader striped highlightOnHover>
 							<Table.Thead>
@@ -92,10 +88,30 @@ const Dashboard = () => {
 					</ScrollArea>
 				</Card>
 
-				<Card withBorder className="h-80"></Card>
-				<Card withBorder className="h-80"></Card>
-				<Card withBorder className="h-80"></Card>
-				<Card withBorder className="h-80"></Card>
+				{/* Fees */}
+				<Card withBorder className="h-80">
+					<Title order={4}>Fees</Title>
+				</Card>
+
+				{/* Total Books */}
+				<Card withBorder className="h-64">
+					<Title order={4}>Total Books</Title>
+				</Card>
+
+				{/* Book Genre */}
+				<Card withBorder className="h-64">
+					<Title order={4}>Book Genres</Title>
+				</Card>
+
+				{/* Book Author */}
+				<Card withBorder className="h-64">
+					<Title order={4}>Top Authors</Title>
+				</Card>
+
+				{/* Recommendations */}
+				<Card withBorder className="h-80 md:col-span-3">
+					<Title order={4}>Recommendations</Title>
+				</Card>
 			</div>
 		</div>
 	);
