50fe66927a64953221aba4448c67dd2f3edd1f5a
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-11-24 13:38:10 -0700

    implemented the forgot password page

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,10 +1,41 @@
 import { useEffect } from "react";
+import { Card, TextInput, Button, Group, Text, Anchor } from "@mantine/core";
+import { Link } from "react-router";
 
 const ForgotPassword = () => {
-  useEffect(() => {
-    document.title = `${import.meta.env.VITE_APP_NAME_ABBREV} | Forgot Password`;
-  }, []);
-  return <div>ForgotPassword</div>;
+	useEffect(() => {
+		document.title = `${
+			import.meta.env.VITE_APP_NAME_ABBREV
+		} | Forgot Password`;
+	}, []);
+
+	return (
+		<div className="h-full flex justify-center items-center">
+			<Card padding="xl" withBorder className="w-sm">
+				<Text size="xl" weight={500} ta="center" mb="sm">
+					Forgot Password
+				</Text>
+
+				<Text mb="sm">
+					Enter your email address below to get a password reset link.
+				</Text>
+				<TextInput
+					label="Email"
+					placeholder="you@example.com"
+					required
+					mb="md"
+				/>
+				<Group position="apart" mb="sm">
+					<Button fullWidth>Send Reset Link</Button>
+				</Group>
+				<Anchor component={Link} to="/account" c="dimmed">
+					<Text size="sm" ta="center">
+						Back to Sign In
+					</Text>
+				</Anchor>
+			</Card>
+		</div>
+	);
 };
 
 export default ForgotPassword;
