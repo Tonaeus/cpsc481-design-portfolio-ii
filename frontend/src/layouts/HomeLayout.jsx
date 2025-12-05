209f3a162d66620b8853fd9e6ef8bc155bb03a33
Rfblaze <angelotroncone@gmail.com>
2025-12-05 11:03:35 -0700

    Removing old navbar

--- frontend/src/layouts/HomeLayout.jsx
+++ frontend/src/layouts/HomeLayout.jsx
@@ -1,12 +1,8 @@
 import { Outlet } from "react-router";
-import Navbar from "../components/Navbar";
 
 function MainLayout() {
 	return (
 		<div className="flex flex-col min-h-screen">
-			<header>
-				<Navbar />
-			</header>
 			<main className="flex justify-center flex-1 bg-gray-100 overflow-y-auto">
         <div className="h-[calc(100vh-56px)] w-full">
 					<div className="">
