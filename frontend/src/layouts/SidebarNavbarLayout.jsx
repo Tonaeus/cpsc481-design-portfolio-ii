342a0bf19a9fe2fb405f931ec9a7cf9be55855e9
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-12-06 23:23:55 -0700

    standardized overflow

--- frontend/src/layouts/SidebarNavbarLayout.jsx
+++ frontend/src/layouts/SidebarNavbarLayout.jsx
@@ -10,24 +10,13 @@ function MainLayout() {
 	const location = useLocation();
 	const path = location.pathname;
 
-	return path === "/" ? (
-		<div className="flex flex-col min-h-screen">
-			<header>{!loading && !user ? <Header /> : <SidebarNavbar />}</header>
-			<main className="flex justify-center flex-1 bg-gray-100 overflow-y-auto">
-				<div className="h-[calc(100vh-56px)] w-full">
-					<div>
-						<Outlet />
-					</div>
-				</div>
-			</main>
-		</div>
-	) : (
+	return (
 		<div className="flex flex-col min-h-screen">
 			<header>
 				<header>{!loading && !user ? <Header /> : <SidebarNavbar />}</header>
 			</header>
 			<main className="flex justify-center flex-1 overflow-auto bg-gray-100">
-				<div className="w-full max-w-7xl p-4">
+				<div className={`w-full max-w-7xl ${path === "/" ? "" : "p-4"}`}>
 					<Outlet />
 				</div>
 			</main>
