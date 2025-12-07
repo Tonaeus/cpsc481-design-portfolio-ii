5abf012d5ea5693e2d8d355132784b2897e7f31a
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-12-06 21:58:48 -0700

    added sidebar to home when logged in

--- frontend/src/layouts/HomeLayout.jsx
+++ frontend/src/layouts/HomeLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import SidebarNavbar from "../components/SidebarNavbar";
+import useAuthContext from "../hooks/useAuthContext";
 
 function MainLayout() {
+	const { state } = useAuthContext();
+	const { loading, user } = state;
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<header>
-				<Header />
+				{(!loading && !user) ? <Header /> : <SidebarNavbar />}
 			</header>
 			<main className="flex justify-center flex-1 bg-gray-100 overflow-y-auto">
 				<div className="h-[calc(100vh-56px)] w-full">
